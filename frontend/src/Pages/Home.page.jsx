0c55b5ed8e91eb1ad4b7df4d12cdc6b473d595c7
rahanyas <rahanyas3@gmail.com>
2025-10-22 19:49:20 +0530

    added layout and desgin to navbar

--- frontend/src/Pages/Home.page.jsx
+++ frontend/src/Pages/Home.page.jsx
@@ -1,9 +1,11 @@
 import { Link } from "react-router-dom";
+import '../styles/page/home.style.scss'
+
 const Home = () => {
     return(
         <div style={{}}>
-            <h1 className="text-blue-500">welcome to world</h1>
-            <Link to='/signup'>signup</Link>
+            <h1 className="home-head">welcome to world</h1>
+            <Link to='/signup' className="signup-link">signup</Link>
         </div>
     )
 };
--- frontend/src/components/Navbar.compo.jsx
+++ frontend/src/components/Navbar.compo.jsx
@@ -1,7 +1,41 @@
+import { useState } from "react";
+import { Link } from "react-router-dom";
+
 const Navbar = ()  => {
+    // dummy user tracker
+    const [isUser, setIsuser] = useState(false)
     return(
-        <div>
-            <h1>this is navbar</h1>
+        <div className="nav">
+            {/* nav first section , includes logo and app name */}
+
+            <div className="nav-col-left">
+               <h1 className="nav-head">hey</h1>
+               <img src="#" alt="hey_logo" />
+            </div>
+
+            {/* nav second section , includes login and profile related btns */}
+
+            <div className="nav-col-right">
+
+                {/* section to show if user is not authenitcated login/signup btns*/}
+
+                  <div className="off-nav-btns">
+
+                    <Link to='/login' className="nav-link">login</Link>
+                    <Link to='/signup' className="nav-link">sign-up</Link>
+
+                  </div>
+
+                  {/* now isUser is just dummy */}
+
+                  {isUser && (
+                    // section to show when user is authenticated
+
+                  <div className="on-nav-btns">
+                    <h1 className="nav-link">profile</h1>
+                  </div>
+                  )}
+            </div>
         </div>
     )
 };
