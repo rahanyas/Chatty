bf1033b783a4bbdbdd9bc1e235761507e8bfa4de
rahanyas <rahanyas3@gmail.com>
2025-11-07 20:26:50 +0530

    creating login page for desktop

--- frontend/src/Pages/LoginPage/Login.Page.jsx
+++ frontend/src/Pages/LoginPage/Login.Page.jsx
@@ -1,8 +1,19 @@
+import {Link} from 'react-router-dom'
+
 import './login.style.scss'
+
 const LoginPage = () => {
     return (
         <div id='container'>
-            lets login
+                <div className="login-box">
+                    <input type="email" name='email' placeholder='email' />
+                    <input type="password" name="password" placeholder='password'/>
+                    <button className='login-btn'>login</button>
+                </div>
+                <div className="oauth-box">
+                    <button className='oauth-Btn'>login with google</button>
+                    <p>dont have an account?<Link to='/signup'>sign-in</Link></p>
+                </div>
         </div>
     )
 };
