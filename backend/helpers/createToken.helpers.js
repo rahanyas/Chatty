6e179070c2dbd705699163a1bc23c75acc97ed92
rahanyas <rahanyas3@gmail.com>
2025-11-27 13:39:01 +0530

    solving cookie error try=5

--- backend/helpers/createToken.helpers.js
+++ backend/helpers/createToken.helpers.js
@@ -17,7 +17,8 @@ export const createToken = (userId, res) => {
     console.log('token created ', token);
 
     res.cookie('token', token, {
-      secure : process.env.NODE_ENV !== 'development' ? true : false,
+      secure : process.env.DEV !== 'development' ? true : false,
+      sameSite : process.env.DEV !== 'development'? 'None' : 'Lax',
       httpOnly  : true, // it will block accessing cookie from client without this anybody can access document.cookie()
       maxAge : 2 * 24 * 60  * 60 * 1000,
     });
